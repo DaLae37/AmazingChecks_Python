b2617fb4c487a6d89518ba163a6453fb67f37d36
DaLae37 <dalae37@gmail.com>
2023-05-18 01:07:19 +0900

    Test unittest library

--- Scenes/multiGameScene.py
+++ Scenes/multiGameScene.py
@@ -9,12 +9,74 @@ from Objects.player import Player
 from Framework.sceneManager import Scene, SceneManager
 from Framework.simpleImage import SimpleImage
 
+from unittest import mock
+
 class MultiGameScene(Scene) :
-       
     def __init__(self, screen, clock) :
         self.screen = screen
         self.clock = clock
 
+        self.load_resources()
+        self.isWhiteTurn = True
+        self.isPlayerSelected = False
+        self.selectedPiece = None
+
+        self.mock = mock.Mock()
+        self.mock.getPlayerSelect.return_value = 0, 1
+        self.mock.getPlayerSelect.side_effect = print("상대방 입력 완료")
+
     def update(self) :
-        print("To Be Continue")
-        SceneManager.getInstance().changeScene("MainScene")
+        self.chessBoard.update(self.screen)
+            
+        isGameEnd = self.chessBoard.isGameEnd()
+        if isGameEnd[0] :
+            if isGameEnd[1] :
+                print("white win!")
+            else :
+                print("black win!")
+            SceneManager.getInstance().changeScene("MainScene")
+        
+        for event in pygame.event.get() :
+            if event.type == pygame.QUIT :
+                SceneManager.getInstance().isQuit = True
+                return
+
+            if not self.isWhiteTurn :
+                x, y =self.mock.getPlayerSelect()
+                print("상대방 선택 X : {}, Y : {}".format(x,y))
+                selectedPiece = self.chessBoard.getPiece(x ,y)
+                self.isWhiteTurn = not self.isWhiteTurn
+                
+            else :
+                if event.type == pygame.MOUSEBUTTONDOWN :
+                    mousePos = pygame.mouse.get_pos()
+                    selectedPiece = self.chessBoard.getPiece(mousePos[0] // 90, mousePos[1] // 90)
+                    if not self.isPlayerSelected :
+                        if (self.isWhiteTurn and self.whitePlayer.inputCheck(selectedPiece))  :
+                                self.selectedPiece =  selectedPiece
+                                self.isPlayerSelected = True
+                                self.chessBoard.setPieceNotation(selectedPiece)
+                                
+                    else :
+                        if self.selectedPiece == selectedPiece :
+                            self.selectedPiece = None
+                            self.isPlayerSelected = False
+                            self.chessBoard.setIsSelected(False)
+                            
+                        else :
+                            if self.chessBoard.movePiece(mousePos[0] // 90, mousePos[1] // 90) :
+                                self.isWhiteTurn = not self.isWhiteTurn
+                            self.selectedPiece = None
+                            self.isPlayerSelected = False
+                            self.chessBoard.setIsSelected(False)
+
+
+    def load_resources(self) :
+        self.chessBoard = ChessBoard()
+        self.whitePlayer = Player(True)
+        self.blackPlayer = Player(False)
+
+        self.whitePlayer.makePieces()
+        self.blackPlayer.makePieces()
+
+        self.chessBoard.makeBoard(self.whitePlayer.getPieces(), self.blackPlayer.getPieces())
--- main.py
+++ main.py
@@ -7,14 +7,40 @@ from Scenes.gameScene import GameScene
 from Scenes.aiGameScene import AIGameScene
 from Scenes.multiGameScene import MultiGameScene
 
+from Objects.ai import AI
+
+import unittest
+
+class ModuleTest(unittest.TestCase) :
+    def test_Inherit(self) :
+        global screen, clock
+        scene = MultiGameScene(screen, clock)
+        scene.load_resources()
+        self.assertRaises(NameError)
+
+    def test_AIGameScene(self) :
+        global screen, clock
+        scene = AIGameScene(screen, clock)
+        scene.load_resources()
+        self.assertIsInstance(scene.blackPlayer, AI)
+
+    def test_MainScene(self) :
+        global screen, clock
+        scene = MainScene(screen, clock)
+        scene.load_resources()
+        self.assertIsNotNone(scene.background)
+
 if __name__ == '__main__' :
     pygame.init()
     pygame.display.set_caption("Amazing Checks")
     pygame.display.set_icon(SimpleImage("Resources/Images/UI/logo.png").getSurface())
     pygame.mixer.pre_init(44100, 16, 2, 4096) #Frequency, Size, Channels, BufferSize
 
+    global screen, clock
     screen = pygame.display.set_mode((720, 720))
     clock = pygame.time.Clock()
+    #unittest.main()
+
     run = True
     
     SceneManager.getInstance().registerGameObjects(screen, clock)
@@ -26,7 +52,6 @@ if __name__ == '__main__' :
 
     while run:
         SceneManager.getInstance().update()
-        
         pygame.display.flip()
         clock.tick(144)
         
